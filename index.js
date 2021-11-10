2fdf373e26066ab4be8c27424aa3bd6373f44b37
caiohscruz <caiohscruz@hotmail.com>
2021-11-10 10:12:16 -0300

    makes calendar index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs')
 mongoose.connect("mongodb://localhost:27017/agendamento", {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get('/', (req, res) =>{
-    res.send("Oi")
+    res.render("index")
 })
 
 app.get('/cadastro', (req, res) =>{
