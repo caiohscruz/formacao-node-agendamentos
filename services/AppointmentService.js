59d818888fbec553ba6e7aba5f97b764edf9cc24
caiohscruz <caiohscruz@hotmail.com>
2021-11-10 15:44:58 -0300

    formats appointments to display in calendar

--- services/AppointmentService.js
+++ services/AppointmentService.js
@@ -1,41 +1,49 @@
-const appointment = require('../models/Appointment')
-const mongoose = require('mongoose')
+const appointment = require("../models/Appointment");
+const mongoose = require("mongoose");
 
-const Appo = mongoose.model("Appointment", appointment)
+const Appo = mongoose.model("Appointment", appointment);
+const AppointmentFactory = require("../factories/AppointmentFactory");
 
 class AppointmentService {
-
-    async Create(name, email, description, cpf, date, time){
-        var newAppo = new Appo({
-            name,
-            email,
-            cpf,
-            description,
-            date,
-            time,
-            finished: false
-        })
-        try{
-            await newAppo.save()
-            return {
-                status: true,
-                msg: "Salvo com sucesso"
-            }
-        }catch(err){
-            return {
-                status: false,
-                msg: "Erro - " +  err
-            }
-        }
+  async Create(name, email, description, cpf, date, time) {
+    var newAppo = new Appo({
+      name,
+      email,
+      cpf,
+      description,
+      date,
+      time,
+      finished: false,
+    });
+    try {
+      await newAppo.save();
+      return {
+        status: true,
+        msg: "Salvo com sucesso",
+      };
+    } catch (err) {
+      return {
+        status: false,
+        msg: "Erro - " + err,
+      };
     }
+  }
 
-    async GetAll(showFinished){
-        if(showFinished==true){
-            return await Appo.find()
-        }else{
-            return await Appo.find({'finished': false})
-        }
+  async GetAll(showFinished) {
+    if (showFinished == true) {
+      var appos = await Appo.find();
+    } else {
+      var appos = await Appo.find({ finished: false });
     }
+    var appointments = [];
+
+    appos.forEach((appointment) => {
+      if (appointment.date != null)
+        appointments.push(AppointmentFactory.Build(appointment));
+    });
+
+    return appointments;
+  }
 }
 
-module.exports= new AppointmentService()
+module.exports = new AppointmentService();
